dbce79f113f2ee26ae09ee2defc14c39d474c807
Davi2803 <160884825+Davi2803@users.noreply.github.com>
2025-05-16 12:14:26 -0300

    .

--- exercicios - 08 do 05/ex32/main.js
+++ exercicios - 08 do 05/ex32/main.js
@@ -1,7 +1,19 @@
 function adicionarTarefa(){
-    const input = document.getElementById("nova-tarefa")
+    const input = document.getElementById('nova-tarefa');
     const textoTarefa = input.value.trim();
-}
 
-if (textoTarefa==="") return;
 
+    if (textoTarefa === "") return;
+
+    const li = document.createElement('li');
+    li.textContent = textoTarefa;
+
+    const botaoRemover = document.createElement('button');
+    botaoRemover.textContent = 'Remover';
+    botaoRemover.onclick = () => li.remove();
+
+    appendChild(botaoRemover); 
+    document.getElementById('lista-tarefas').appendChild(li);
+
+    input.value = '';
+ }    
